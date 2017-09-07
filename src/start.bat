bbb5ad70bc3c78f6fe43f49135e899bc27722c64
AngheloAlf10 <angheloalf95@gmail.com>
2017-09-07 16:46:42 -0300

    a bit of bat

--- src/start.bat
+++ src/start.bat
@@ -1,6 +1,7 @@
 @echo off
 title start_bt3_character_unk_editor
-@echo Ejecutando programa
+echo Ejecutando programa
 bt3_character_unk_editor.exe
+echo %errorlevel%
 pause
-exit
+exit /b %errorlevel%
