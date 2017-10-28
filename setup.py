97553309a665fdb65643315771f597e35acdd327
AngheloAlf10 <angheloalf95@gmail.com>
2017-10-27 21:29:10 -0300

    Actualizando datos

--- setup.py
+++ setup.py
@@ -331,6 +331,7 @@ def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
         for file in files:
             ziph.write(os.path.join(root, file))
+    return
 
 
 def copyPythonDependencies():
