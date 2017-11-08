0d8bc90ac246d6128dfa540089eb34a4901a803f
AngheloAlf10 <angheloalf95@gmail.com>
2017-11-08 19:15:31 -0300

    wea comentada

--- src/packages/StatChars.py
+++ src/packages/StatChars.py
@@ -21,6 +21,9 @@ class StatChars:
             print("\t\t\t\ttype:", self.type, self.type.decode("utf-16"))
             print("\t\t\t\ttextType:", self.textType, self.textType.decode("utf-16"))
             print("\t\t\t\ttext:", self.text, self.text.decode("utf-16"))
+        # if b"=\x00" in self.text:
+        #     wea = self.text.split(b"=\x00")[1][:-4]
+        #     print(wea.decode("utf-16"), len(wea)/2)
         return
 
     def getUnicodeList(self):
